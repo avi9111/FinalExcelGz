41dcab596a92e2b3faf4e1ff1369429fde111670
DESKTOP-DEBHPTM\ll <avi9111@163.com>
2022-07-13 02:39:48 +0800

    提交Excel 工具，之後會換成litjson

---
+++ Assets/ExcelTask.cs
@@ -0,0 +1,16 @@
+//
+// Auto Generated Code By excel2json
+// https://neil3d.gitee.io/coding/excel2json.html
+// 1. 每个 Sheet 形成一个 Struct 定义, Sheet 的名称作为 Struct 的名称
+// 2. 表格约定：第一行是变量名称，第二行是变量类型
+
+// Generate From D:\Tools\excel2json\tasks.xlsx.xlsx
+
+public class ExcelTask
+{
+    public int TaskId; // 人物id
+    public string Desc; // 任务
+}
+
+
+// End of Auto Generated Code
